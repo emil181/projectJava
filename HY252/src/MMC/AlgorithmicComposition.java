505a3302ec19d4147e65ae0b0a73bb6210947e16
emil181 <emilios_94@hotmail.gr>
2014-01-10 01:51:21 +0200

    algorComp

---
+++ HY252/src/MMC/AlgorithmicComposition.java
@@ -0,0 +1,28 @@
+package MMC;
+
+public class AlgorithmicComposition extends Composition {
+
+	public AlgorithmicComposition(String compName, String composer) {
+		super(compName, composer);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AlgorithmicComposition(String compName, String composer,
+			String instrument) {
+		super(compName, composer, instrument);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AlgorithmicComposition(String compName, String composer,
+			String instrument, String notes) {
+		super(compName, composer, instrument, notes);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	void compose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
